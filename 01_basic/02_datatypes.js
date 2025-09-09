e7e77093b9817d01169f600f537761d5c75929f2
P-r-e-e-t-i23 <preeti064btmae23@igdtuw.ac.in>
2025-09-09 11:10:36 +0000

    datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we are using nodejs,not browser
+console.log(3
+    +
+    3)//code readability shold be high
+console.log("preeti")
+let name ="preeti"
+let age=21
+let isLoggedIn =false
+let state;
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object
+
+console.log(typeof null);
